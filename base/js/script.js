36e256a2aef271fc2b4a46a4c3c5b9ebf0f10c02
是一只栗子啊 <64683147+Chairowell@users.noreply.github.com>
2022-09-24 20:40:24 +0800

    Add files via upload

---
+++ base/js/script.js
@@ -0,0 +1,12 @@
+// 网页加载完成时
+window.onload = function () {
+    document.title = 'Z/X Fighting ~';
+}
+// 网页获取焦点时
+window.onfocus = function () {
+    document.title = 'Z/X Fighting ~';
+};
+// 网页失去焦点时
+window.onblur = function () {
+    document.title = '请勿离开哦！';
+};
